d6bf4a23afa2561088e85696d8aae930e8ab48f6
Pavel Smelov <pavsmel@mail.ru>
2018-11-12 17:12:46 +0200

    Add files via upload

--- my_spheres.py
+++ my_spheres.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-from yade import pack,ymport,export,geom,bodiesHandling, qt
+from yade import pack, ymport, export, geom, bodiesHandling, qt
 import math
 import os
 
@@ -13,18 +13,25 @@ class Material:
         self.frictionAngle = frictionAngle            #in radians
         self.density = density                        #[kg/m**3]
 
-
 catalog = {}
 #Other materials can be added here
 catalog['Ni'] = Material(2e8, 0.3, 0.5, 8e3)
 
-sample = catalog[sys.argv[1]]                          #studied material
+sample = catalog[sys.argv[1]]     #studied material
 
 boxHeight = 500
 boxLength = 1000
-boxWidth = 100
-gap = 50                           #distance between bottom of the box and knife
-velocity = 5                       #velocity of knife; can be variated
+boxWidth = 300
+gap = 50                          #distance between bottom of the box and knife
+knifeVelocity = 5                 #velocity of knife; better not be variated for a while
+                                  #knife moves from the start, delay hasn't been made yet
+
+def currentVelocity(knifeVelocity, bollean = True):
+    if bollean:
+        print(bollean)
+        return knifeVelocity
+    else:
+        return 0
 
 Diams = [10.0, 22.14, 31.53, 48.55, 58.23, 100.0]
 massCum = [       0.10, 0.50 , 0.90 , 0.95 , 1.0]
@@ -58,34 +65,39 @@ Knifes.append(O.bodies.append(yade.geom.facetBox([-boxLength/10, boxWidth/2, (bo
                             color = [1, 0, 0]
                             )))
 
-knifeMovement = TranslationEngine(translationAxis=[1,0,0],velocity=velocity,ids=Knifes[0])
-
-v.eyePosition = Vector3(boxLength/2,-boxWidth*10, boxHeight*0.7)
+v.eyePosition = Vector3(boxLength/2,-boxWidth*3, boxHeight*0.7)
 v.viewDir = Vector3(0, 1, -0.35*boxHeight/boxLength)
 v.sceneRadius = boxLength
 v.screenSize = Vector2i(1280, 760)
 
-factory = BoxFactory(maxParticles=3000, maxMass=-1,
-        extents=(boxLength/2, boxWidth/2, 1000), center=(boxLength/2, boxWidth/2, 1000),
-        vMin=20.0, vMax=25.0,
-		PSDsizes=Diams, PSDcum=massCum, PSDcalculateMass=True, exactDiam=False,
-		vAngle=0, massFlowRate=2e12, normal=(0.0,0.0,-1.0), label='factory',
-        mask=0b11, silent=True, stopIfFailed=False
+factory = BoxFactory(maxParticles = 5000, maxMass = -1,
+        center = (boxLength/2, boxWidth/2, 2*boxHeight),
+        extents = (boxLength/2, boxWidth/2, 2*boxHeight),
+        vMin = 20.0, vMax = 25.0,
+		PSDsizes = Diams, PSDcum = massCum,
+        PSDcalculateMass = True, exactDiam = False,
+		vAngle = 0, massFlowRate = 2e12,
+        normal = (0.0,0.0,-1.0), label = 'factory',
+        mask = 0b11, silent = True, stopIfFailed = False
     )
 
-
 O.engines=[
-   ForceResetter(),
-   InsertionSortCollider([Bo1_Sphere_Aabb(), Bo1_Facet_Aabb()], verletDist = Diams[0]),
-   InteractionLoop(
-      [Ig2_Sphere_Sphere_ScGeom(), Ig2_Facet_Sphere_ScGeom()],
-      [Ip2_FrictMat_FrictMat_FrictPhys()],
-      [Law2_ScGeom_FrictPhys_CundallStrack()]
-   ),
-   NewtonIntegrator(gravity=(0,0,-9.81),damping=0.1),
-   factory,
-   knifeMovement
+    ForceResetter(),
+    InsertionSortCollider([Bo1_Sphere_Aabb(), Bo1_Facet_Aabb()], verletDist = Diams[0]),
+    InteractionLoop(
+        [Ig2_Sphere_Sphere_ScGeom(), Ig2_Facet_Sphere_ScGeom()],
+        [Ip2_FrictMat_FrictMat_FrictPhys()],
+        [Law2_ScGeom_FrictPhys_CundallStrack()]
+    ),
+    NewtonIntegrator(gravity = (0, 0, -9.81), damping = 0.1),
+    factory,
+    PyRunner(iterPeriod = 1, command = 'knifeVelocity, factory.numParticles == factory.maxParticles'),
+    TranslationEngine(translationAxis = [1, 0, 0],
+                      velocity = currentVelocity(knifeVelocity),
+                      ids = Knifes[0]),
+    GlobalStiffnessTimeStepper(timeStepUpdateInterval = 100, timestepSafetyCoefficient = 0.8,
+                               defaultDt = 0.0005)
 ]
 
-O.dt= 5e-2
+#O.dt= 5e-2
 O.saveTmp()
